fc3d392151db011e8fb6667f1eb2ad69e749bde2
Suyash Koltharkar <suyashkoltharkar@gmail.com>
2021-07-07 16:35:46 +0530

    afa

--- example/lib/Screens/CaesarEncrypt.dart
+++ example/lib/Screens/CaesarEncrypt.dart
@@ -11,10 +11,15 @@ class CaesarEncrypt extends StatefulWidget {
 
 class _CaesarEncryptState extends State<CaesarEncrypt> {
   int currentTab = 0;
+  final _plainTextController = TextEditingController();
+  final _cipherTextController = TextEditingController();
+  String dropDownShift = '0';
+  List<String> numbers = new List<String>.generate(26, (i) => '$i');
   @override
   Widget build(BuildContext context) {
     Size size = getSizeOfContext(context);
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -71,7 +76,6 @@ class _CaesarEncryptState extends State<CaesarEncrypt> {
           ),
           Expanded(
             child: Container(
-              child: Text("sbfs"),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(45),
@@ -80,31 +84,253 @@ class _CaesarEncryptState extends State<CaesarEncrypt> {
                 color: MyColors.boxBackground,
               ),
               width: size.height,
+              child: currentTab == 0
+                  ? Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: size.height * 0.08),
+                          padding: EdgeInsets.all(14),
+                          child: TextField(
+                              controller: _plainTextController,
+                              decoration: InputDecoration(
+                                  filled: true,
+                                  hoverColor: Colors.transparent,
+                                  fillColor: Colors.white,
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        width: 1, color: MyColors.white),
+                                  ),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(4)),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        width: 1, color: MyColors.white),
+                                  ),
+                                  hintStyle: TextStyle(
+                                      fontSize: 12, color: Color(0xffB3B3B3)),
+                                  // hintText: 'abc@def.com',
+                                  hintText: "Enter a Plain Text")),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(14),
+                          child: Row(
+                            children: [
+                              Text(
+                                "Shift :",
+                                style: TextStyle(
+                                    color: MyColors.white, fontSize: 20),
+                              ),
+                              SizedBox(
+                                width: size.width * 0.02,
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(right: 25),
+                                margin:
+                                    EdgeInsets.only(left: size.width * 0.01),
+                                child: DropdownButton<String>(
+                                  style: TextStyle(color: Color(0xff9D9D9D)),
+                                  value: dropDownShift,
+                                  // style: TextStyle(color: Colors.deepPurple),
+                                  onChanged: (String newValue) {
+                                    setState(() {
+                                      dropDownShift = newValue;
+                                    });
+                                  },
+                                  items: numbers.map<DropdownMenuItem<String>>(
+                                      (String value) {
+                                    return DropdownMenuItem<String>(
+                                      value: value,
+                                      child: Text(value),
+                                    );
+                                  }).toList(),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: size.height * 0.03,
+                        ),
+                        Container(
+                          width: size.width * 0.3,
+                          height: 45,
+                          alignment: Alignment.centerRight,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(4),
+                              color: widget.color),
+                          child: Center(
+                              child: Text(
+                            "Encrypt",
+                            style: TextStyle(
+                                color: MyColors.background,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600),
+                          )),
+                        ),
+                        SizedBox(
+                          height: size.height * 0.03,
+                        ),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          padding: EdgeInsets.all(14),
+                          child: Text(
+                            "Cipher Generated :",
+                            style:
+                                TextStyle(color: MyColors.white, fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(
+                          height: size.height * 0.02,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(14),
+                          child: TextField(
+                              enabled: false,
+                              decoration: InputDecoration(
+                                  filled: true,
+                                  hoverColor: Colors.transparent,
+                                  fillColor: Colors.white,
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        width: 1, color: MyColors.white),
+                                  ),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(4)),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        width: 1, color: MyColors.white),
+                                  ),
+                                  hintStyle: TextStyle(
+                                      fontSize: 12, color: Color(0xffB3B3B3)),
+                                  // hintText: 'abc@def.com',
+                                  hintText: "")),
+                        ),
+                      ],
+                    )
+                  : Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: size.height * 0.08),
+                          padding: EdgeInsets.all(14),
+                          child: TextField(
+                              controller: _cipherTextController,
+                              decoration: InputDecoration(
+                                  filled: true,
+                                  hoverColor: Colors.transparent,
+                                  fillColor: Colors.white,
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        width: 1, color: MyColors.white),
+                                  ),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(4)),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        width: 1, color: MyColors.white),
+                                  ),
+                                  hintStyle: TextStyle(
+                                      fontSize: 12, color: Color(0xffB3B3B3)),
+                                  // hintText: 'abc@def.com',
+                                  hintText: "Enter a Cipher Text")),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(14),
+                          child: Row(
+                            children: [
+                              Text(
+                                "Shift :",
+                                style: TextStyle(
+                                    color: MyColors.white, fontSize: 20),
+                              ),
+                              SizedBox(
+                                width: size.width * 0.02,
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(right: 25),
+                                margin:
+                                    EdgeInsets.only(left: size.width * 0.01),
+                                child: DropdownButton<String>(
+                                  style: TextStyle(color: Color(0xff9D9D9D)),
+                                  value: dropDownShift,
+                                  // style: TextStyle(color: Colors.deepPurple),
+                                  onChanged: (String newValue) {
+                                    setState(() {
+                                      dropDownShift = newValue;
+                                    });
+                                  },
+                                  items: numbers.map<DropdownMenuItem<String>>(
+                                      (String value) {
+                                    return DropdownMenuItem<String>(
+                                      value: value,
+                                      child: Text(value),
+                                    );
+                                  }).toList(),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: size.height * 0.03,
+                        ),
+                        Container(
+                          width: size.width * 0.3,
+                          height: 45,
+                          alignment: Alignment.centerRight,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(4),
+                              color: widget.color),
+                          child: Center(
+                              child: Text(
+                            "Decrypt",
+                            style: TextStyle(
+                                color: MyColors.background,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600),
+                          )),
+                        ),
+                        SizedBox(
+                          height: size.height * 0.03,
+                        ),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          padding: EdgeInsets.all(14),
+                          child: Text(
+                            "Plain text Generated :",
+                            style:
+                                TextStyle(color: MyColors.white, fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(
+                          height: size.height * 0.02,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(14),
+                          child: TextField(
+                              enabled: false,
+                              decoration: InputDecoration(
+                                  filled: true,
+                                  hoverColor: Colors.transparent,
+                                  fillColor: Colors.white,
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        width: 1, color: MyColors.white),
+                                  ),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(4)),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        width: 1, color: MyColors.white),
+                                  ),
+                                  hintStyle: TextStyle(
+                                      fontSize: 12, color: Color(0xffB3B3B3)),
+                                  // hintText: 'abc@def.com',
+                                  hintText: "")),
+                        ),
+                      ],
+                    ),
             ),
           ),
-          // Stack(
-          //   children: [
-          //     Container(
-          //       height: MediaQuery.of(context).size.height -82.0,
-          //       width: MediaQuery.of(context).size.height,
-          //       color: Colors.transparent,
-          //     ),
-          //     Positioned(
-          //       top: 75,
-          //       child: Container(
-          //         decoration: BoxDecoration(
-          //           borderRadius: BorderRadius.only(
-          //             topLeft: Radius.circular(45),
-          //             topRight: Radius.circular(45),
-          //           ),
-          //           color: MyColors.boxBackground,
-          //         ),
-          //         height: MediaQuery.of(context).size.height -100,
-          //       width: MediaQuery.of(context).size.height,
-          //       ),
-          //     )
-          //   ],
-          // )
         ],
       ),
     );
